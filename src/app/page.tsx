5d947c765232166424cb29ac195acc5c709248cb
avettoretto <alessio.ale98@yahoo.it>
2026-02-24 16:00:28 +0100

    Modifiche varie 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 import { getAllTrips } from "@/lib/trips";
 import { HeroSection } from "@/components/home/HeroSection";
 import { TripList } from "@/components/home/TripList";
-import { ForestBorders } from "@/components/home/ForestBorders";
 
 export default function HomePage() {
   const trips = getAllTrips();
 
   return (
-    <div className="relative">
-      <ForestBorders />
-      <div className="relative z-10">
-        <HeroSection />
-        <TripList trips={trips} />
-      </div>
-    </div>
+    <>
+      <HeroSection />
+      <TripList trips={trips} />
+    </>
   );
 }
